b890275dd2cc5d2aea1eef62e4c825d4840afb01
JavaEE <kamaldamir2016@mail.ru>
2024-03-01 02:33:21 +0300

    spring-library

--- src/main/java/ru/maxima/springsecuritylibrary/security/SecurityConfig.java
+++ src/main/java/ru/maxima/springsecuritylibrary/security/SecurityConfig.java
@@ -3,6 +3,7 @@ package ru.maxima.springsecuritylibrary.security;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -43,6 +44,7 @@ public class SecurityConfig {
                         .failureUrl("/auth/login?error"));
         return http.build();
     }
+
 
     @Bean
     public PasswordEncoder getPasswordEncoder(){
